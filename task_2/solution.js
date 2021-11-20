79066781df93717561ff5abc8558add648bbcae8
Ekaterina Sidorova <sevwgl@gmail.com>
2021-11-21 00:28:26 +0300

    task 2.2 done

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,16 +6,22 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
 
     // создайте переменную shippingSum
-
+    let shippingSum;
     // если productsSum равно 0,
+    if (productsSum == 0) {
     // то shippingSum присвоить значение 0
-
+        shippingSum = 0;
+    }    
     // если productsSum Больше или равна freeShippingMinSum,
+    if (productsSum >= freeShippingMinSum) {
     // то shippingSum присвоить значение 0
-
+       shippingSum = 0 ;
+    }       
     // если productsSum больше 0 и меньше freeShippingMinSum,
+    if (productsSum > 0 && productsSum < freeShippingMinSum) {
     // то shippingSum присвоить значение shippingPrice
-
+        shippingSum = shippingPrice ; 
+    }    
     // Конец решения задания №2.1.
 
     return shippingSum;
@@ -29,11 +35,15 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
-
+    let discountSum;
     // если productsSum больше или равно discountMinSum,
+    if (productsSum >= discountMinSum) {
     // то присвойте discountSum значение discountPart процентов от productsSum,
+        (discountSum = productsSum / 100 * discountPart);
+    } else {
     // иначе присвойте discountSum значение 0
-
+        discountSum = 0;
+    }
     // Конец решения задания №2.2.
 
     return discountSum;
